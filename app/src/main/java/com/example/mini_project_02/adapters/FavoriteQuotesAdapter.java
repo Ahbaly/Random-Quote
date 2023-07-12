5308f3349017702c50e094c0dbf65dd9e9f70fcb
Ahbaly <123125056+Ahbaly@users.noreply.github.com>
2023-07-12 01:34:37 +0100

    customize RV with spannable

--- app/src/main/java/com/example/mini_project_02/adapters/FavoriteQuotesAdapter.java
+++ app/src/main/java/com/example/mini_project_02/adapters/FavoriteQuotesAdapter.java
@@ -1,6 +1,17 @@
 package com.example.mini_project_02.adapters;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +29,14 @@ public class FavoriteQuotesAdapter extends RecyclerView.Adapter<FavoriteQuotesAd
     private ArrayList<Quote> quotes;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView tvFavQuoteItemInfos;
+        private final TextView tvQuote;
+        private final TextView tvAuthor;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            tvFavQuoteItemInfos = itemView.findViewById(R.id.tvFavQuoteItemInfos);
+            tvQuote = itemView.findViewById(R.id.tvQuote);
+            tvAuthor = itemView.findViewById(R.id.tvAuthor);
         }
     }
 
@@ -42,8 +55,53 @@ public class FavoriteQuotesAdapter extends RecyclerView.Adapter<FavoriteQuotesAd
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Quote quote = quotes.get(position);
+        Spannable spannableQuote = new SpannableString(quote.getQuote());
+        Spannable spannableAuthor = new SpannableString(quote.getAuthor());
 
-        holder.tvFavQuoteItemInfos.setText(quote.infos());
+//        QUOTE : SIZE
+        spannableQuote.setSpan(new AbsoluteSizeSpan(16, true),
+                0,
+                spannableQuote.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        spannableQuote.setSpan(new AbsoluteSizeSpan(20, true),
+                0,
+                1,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+//        QUOTE : TEXT COLOR
+        spannableQuote.setSpan(new ForegroundColorSpan(Color.BLUE),
+                0,
+                1,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+//        AUTHOR : SIZE
+        spannableAuthor.setSpan(new AbsoluteSizeSpan(13, true),
+                0,
+                spannableAuthor.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+//        AUTHOR : TEXT COLOR
+        spannableAuthor.setSpan(new ForegroundColorSpan(Color.GRAY),
+                0,
+                spannableAuthor.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+//        AUTHOR : UNDERLINE
+        spannableAuthor.setSpan(new UnderlineSpan(),
+                0,
+                spannableAuthor.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+//        AUTHOR : BOLD
+        spannableAuthor.setSpan(new StyleSpan(Typeface.BOLD),
+                0,
+                spannableAuthor.length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        holder.tvQuote.setText(spannableQuote);
+        holder.tvAuthor.setText(spannableAuthor);
     }
 
     @Override
